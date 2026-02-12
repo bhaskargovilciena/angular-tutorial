7cc5876ed4b4bce9594c11f70ac38a41969404e3
bgovil <bgovil@ciena.com>
2026-02-12 15:40:41 +0530

    some basic changes in the project to understand the application and various files

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-tutorial');
+  title = "Angular Tutorial";
+  name = "Bhaskar Govil"
 }
