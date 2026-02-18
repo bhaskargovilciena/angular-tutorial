7289225e7b47a4f1a15eb081b6c047d115fdbcb9
bgovil <bgovil@ciena.com>
2026-02-18 12:39:20 +0530

    product reviews component added

---
+++ src/app/components/product-reviews/product-reviews.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-reviews',
+  imports: [],
+  templateUrl: './product-reviews.html',
+  styleUrl: './product-reviews.css',
+})
+export class ProductReviews {
+
+}
