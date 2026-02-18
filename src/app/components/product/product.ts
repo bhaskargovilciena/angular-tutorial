fcee04fbea3821c7bb4766aef532d53e623580e4
bgovil <bgovil@ciena.com>
2026-02-18 12:38:51 +0530

    product component added

---
+++ src/app/components/product/product.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-product',
+  imports: [RouterOutlet],
+  templateUrl: './product.html',
+  styleUrl: './product.css',
+})
+export class Product {
+  
+}
