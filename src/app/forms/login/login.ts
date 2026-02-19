f07dbf4ad51888c6107b3a8d718343f6dfa497bb
bgovil <bgovil@ciena.com>
2026-02-19 16:15:12 +0530

    form creating using FormField importer and form function. some basic reads also performed.

--- src/app/forms/login/login.ts
+++ src/app/forms/login/login.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { form, FormField } from '@angular/forms/signals';
+
+interface LoginInterface {
+  username:string,
+  password:string
+}
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormField],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-  
+  loginModel = signal<LoginInterface>({
+    username:'',
+    password:''
+  })
+
+  loginForm = form(this.loginModel)
 }
